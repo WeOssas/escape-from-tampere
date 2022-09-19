7a7fdb7298964f2f01d8a2bbbe30b771b3422793
Nilongg <niilo.vanhatupa@outlook.com>
2022-09-19 10:19:41 +0300

    Soldier patrol animation

--- Assets/Scripts/Soldier/SoldierAi.cs
+++ Assets/Scripts/Soldier/SoldierAi.cs
@@ -5,8 +5,9 @@ using UnityEngine.AI;
 
 public class SoldierAi : MonoBehaviour
 {
-
+    // Static is made just in case.
     public static SoldierAi instance;
+    
     public Animator anim;
     public NavMeshAgent agent;
 
@@ -17,6 +18,7 @@ public class SoldierAi : MonoBehaviour
     private int timesToShoot = 5;
     public bool gotShot;
 
+    public float pathfindingRange;
 
     void Shoot()
     {
@@ -46,6 +48,7 @@ public class SoldierAi : MonoBehaviour
         if (!agent.hasPath)
         {
             agent.SetDestination(SearchPath());
+            anim.SetBool("Patrol", true);
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -58,13 +61,29 @@ public class SoldierAi : MonoBehaviour
 
     private Vector3 SearchPath()
     {
-        Vector3 newDest = SoldierPatrol.instance.GetDestinationSuggestion();
+        Vector3 newDest = GetDestinationSuggestion();
         return newDest;
     }
 
 
+    /// <returns>
+    /// A destination suggestion that will be used if it is determined valid. May be null (in which case the suggestion won't be valid).
+    /// </returns>
+    public Vector3 GetDestinationSuggestion()
+    {
+        // TODO: Implement pathfinding across different Y coordinates.
+        float randomX = Random.Range(-pathfindingRange, pathfindingRange);
+        float randomZ = Random.Range(-pathfindingRange, pathfindingRange);
 
-   
+        return transform.position + new Vector3(randomX, 0f, randomZ);
+    }
+
+    public void ChaseAndShoot()
+    {
+        anim.SetBool("GotShot", true);
+    }
+
+
 
 
 }
