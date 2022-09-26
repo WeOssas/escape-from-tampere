610371b6fc6cf8b1d90aefd9d1dd927ab1be4d52
Nilongg <niilo.vanhatupa@outlook.com>
2022-09-26 09:55:20 +0300

    Koodin muokkaamista

--- Assets/Scripts/Soldier/SoldierAi.cs
+++ Assets/Scripts/Soldier/SoldierAi.cs
@@ -26,7 +26,76 @@ public class SoldierAi : MonoBehaviour
     public float pathfindingRange;
     public float maxChasingRange;
 
-  
+    private void AttackOrPatrol()
+    {
+        //If soldier got shot and player is in attack range
+        if (gotShot && inAttackRange)
+        {
+            ChaseAndShoot();
+        }
+
+        //Patrol if soldier haven't been shot or doesn't have a path or player isn't in attack range
+        else if (!gotShot && !agent.hasPath && !inAttackRange)
+        {
+            agent.SetDestination(SearchPath());
+            anim.SetBool("Patrol", true);
+        }
+    }
+
+    public void ChaseAndShoot()
+    {
+
+        if (agent.SetDestination(GetChasingSuggestion()) == false)
+        {
+            gotShot = false;
+            inAttackRange = false;
+            return;
+        }
+
+        transform.LookAt(PlayerInstance.instance.transform);
+        for (int i = 0; i < timesToShoot; i++)
+        {
+            Shoot();
+        }
+
+    }
+
+    public void Shoot()
+    {
+        /*
+        Play an attack animation and instantiate a bullet from shooting positioning (named shootPos in hierachy)
+        and add force to the bullet aiming to the player's current position.
+        */
+        
+        anim.Play("attack");
+        GameObject newBullet = Instantiate(gunBullet, shootPos.position, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody>().AddForce(playerPos.transform.position * 100, ForceMode.Impulse);
+
+    }
+
+    public Vector3 GetDestinationSuggestion()
+    {
+        // TODO: Implement pathfinding across different Y coordinates.
+        float randomX = Random.Range(-pathfindingRange, pathfindingRange);
+        float randomZ = Random.Range(-pathfindingRange, pathfindingRange);
+
+        return transform.position + new Vector3(randomX, 0f, randomZ);
+    }
+
+    private Vector3 SearchPath()
+    {
+        Vector3 newDest = GetDestinationSuggestion();
+        return newDest;
+    }
+
+
+    Vector3 GetChasingSuggestion()
+    {
+        Vector3 playerPos = PlayerInstance.instance.transform.position;
+
+        // If the player is in range, return player position, else null.
+        return Vector3.Distance(transform.position, playerPos) <= maxChasingRange ? playerPos : Vector3.zero;
+    }
     private void Awake()
     {
         instance = this;
@@ -50,72 +119,20 @@ public class SoldierAi : MonoBehaviour
         }
     }
 
-    private Vector3 SearchPath()
-    {
-        Vector3 newDest = GetDestinationSuggestion();
-        return newDest;
-    }
-
+    
 
     /// <returns>
     /// A destination suggestion that will be used if it is determined valid. May be null (in which case the suggestion won't be valid).
     /// </returns>
-    public Vector3 GetDestinationSuggestion()
-    {
-        // TODO: Implement pathfinding across different Y coordinates.
-        float randomX = Random.Range(-pathfindingRange, pathfindingRange);
-        float randomZ = Random.Range(-pathfindingRange, pathfindingRange);
+   
 
-        return transform.position + new Vector3(randomX, 0f, randomZ);
-    }
+    
 
-    public void ChaseAndShoot()
-    {
-        
-        if(agent.SetDestination(GetChasingSuggestion()) == false)
-        {
-            gotShot = false;
-            inAttackRange = false;
-            return;
-        }
+    
 
-        transform.LookAt(PlayerInstance.instance.transform);
-        for(int i = 0; i < timesToShoot; i++)
-        {
-            shoot();
-        }
+  
 
-    }
-
-    Vector3 GetChasingSuggestion()
-    {
-        Vector3 playerPos = PlayerInstance.instance.transform.position;
-
-        // If the player is in range, return player position, else null.
-        return Vector3.Distance(transform.position, playerPos) <= maxChasingRange ? playerPos : Vector3.zero;
-    }
-
-    public void shoot()
-    {
-        anim.Play("attack");
-        GameObject newBullet = Instantiate(gunBullet, shootPos.position, Quaternion.identity);
-        newBullet.GetComponent<Rigidbody>().AddForce(playerPos.transform.position * 100, ForceMode.Impulse);
-
-    }
-
-    private void AttackOrPatrol()
-    {
-        if (gotShot && inAttackRange)
-        {
-            ChaseAndShoot();
-        }
-
-        else if (!gotShot && !agent.hasPath && !inAttackRange)
-        {
-            agent.SetDestination(SearchPath());
-            anim.SetBool("Patrol", true);
-        }
-    }
+    
 
 
 }
